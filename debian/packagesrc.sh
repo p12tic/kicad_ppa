59ce6a545dd99b6e6a96ec3682eb4a6ddb994259
Adam Wolf <adamwolf@feelslikeburning.com>
2011-05-06 14:03:12 -0500

    updated from upstream packaging stuff.

--- debian/packagesrc.sh
+++ debian/packagesrc.sh
@@ -1,37 +1,17 @@
 #!/bin/bash
 
-ver=`cat ../trunk/debian/changelog | head -n1 | cut -f2 -d\( | cut -f1 -d-`
-debver=`cat ../trunk/debian/changelog | head -n1 | cut -f2 -d- | cut -f1 -d\)`
-svnpath="../../trunk" #extra .. as we'll be down another dir when used
-signer="richardaburton@gmail.com"
+d=kicad-0.0.$(date +%Y%m%d).orig
+tgz=kicad_0.0.$(date +%Y%m%d).orig.tar.gz
+dd=${d/.orig/}
+mkdir $d
+bzr export $d/kicad lp:kicad/stable 
+bzr export $d/kicad-doc lp:~kicad-developers/kicad/doc
+bzr export $d/kicad-library lp:~kicad-lib-committers/kicad/library
+cp -a $d $dd
+tar czf $tgz $d && rm -rf $d
 
-mkdir kicad-${ver}.orig
-
-if [ "${debver}" = "1" ]; then
-
-    cd kicad-${ver}.orig
-    svn export ${svnpath}/kicad
-    svn export ${svnpath}/kicad-doc
-    svn export ${svnpath}/kicad-library
-    # remove any unwanted stuff
-    rm -rf kicad/kicad/minizip
-    rm -rf kicad-doc/doc/help/{de,es,it}
-    rm -rf kicad-doc/presentations
-    cd ..
-
-    cp -a kicad-${ver}.orig kicad-${ver}
-
-else
-    tar zxf kicad_${ver}.orig.tar.gz
-    mv kicad_${ver}.orig kicad_${ver}
-fi
-
-cd kicad-${ver}
-svn export ${svnpath}/debian
-cd ..
-
-dpkg-source -b kicad-${ver}
-debsign -m${signer} kicad_${ver}-${debver}.dsc
-
-#rm -rf kicad-${ver}
+# remove any unwanted stuff
+rm -rf $dd/kicad/kicad/minizip
+#rm -rf $dd/kicad-doc/doc/help/{de,es,it}
+rm -rf $dd/kicad-doc/presentations
 
